733f466237302845d5e7e5d22a1ff0ca89f05924
nakulrandad <nakulrandad@gmail.com>
2023-07-23 04:13:18 +0530

    add package metadata

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,4 +1,8 @@
 """Main quant package
 """
 
-from quant.quant_accessor import QuantDataFrameAccessor
+from . import version
+from .quant_accessor import QuantDataFrameAccessor
+
+__version__ = version.version
+__author__ = "Nakul Randad"
